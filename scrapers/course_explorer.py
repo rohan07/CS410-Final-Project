1fc27f149e318392adcbac6e90148851eb3a36f4
Genevieve <genevieve.helsel@gmail.com>
2018-05-02 16:57:23 -0500

    course explorer start

---
+++ scrapers/course_explorer.py
@@ -0,0 +1,12 @@
+'''
+to get department codes:
+https://courses.illinois.edu/schedule/2018/fall
+
+for each department code, to get course codes:
+https://courses.illinois.edu/schedule/2018/fall/AAS
+
+for each course, to get instructor (and other course information)
+https://courses.illinois.edu/schedule/2018/fall/AAS/100
+
+TODO: look at https://courses.illinois.edu/cisdocs/explorer
+'''
