f87f9d545fc0bd16b3656d3b697898439d753828
Genevieve <genevieve.helsel@gmail.com>
2018-05-02 14:59:01 -0500

    koofers professors, sorta? it find 2k more than there are somehow?

--- scrapers/koofers.py
+++ scrapers/koofers.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-
 course_info = {}
 # get course info
 for pg in range(1,170):
@@ -12,6 +11,7 @@ for pg in range(1,170):
     soup = BeautifulSoup(data)
 
     classes = soup.findAll("div", {"onclick" : re.compile("document.location = '/university-of-illinois-urbana.*?")})
+    i = 0
     for c in classes:
 
         if c.text == "(adsbygoogle = window.adsbygoogle || []).push({});":
@@ -37,7 +37,7 @@ for pg in range(1,170):
 
         num_ratings = c.find("div", {"class": "rating_link"})
         if num_ratings:
-            num_ratings = num_ratings.text.strip()
+            num_ratings = num_ratings.text.strip()[:-8]
         else:
             num_ratings = ""
 
@@ -47,7 +47,9 @@ for pg in range(1,170):
         else:
             gpa = ""
 
-        course_info[number] = {"name":name, "rating":rating, "num_ratings":num_ratings, "gpa":gpa}
+        # course_info[number]
+        course_info["{}-{}".format(pg,i)] = {"name":name, "number":number, "rating":rating, "num_ratings":num_ratings, "gpa":gpa}
+        i += 1
 
     if pg%10 == 0:
         print("On page {}".format(pg))
@@ -58,8 +60,55 @@ print("Number of classes found: {}".format(len(course_info)))
 with open('koofers_courses.json', 'w') as fp:
     json.dump(course_info, fp)
 
+
+professor_info = {}
 # get professor info
-# for pg in range(1,170):
-#     page = requests.get('https://www.koofers.com/university-of-illinois-urbana-champaign-uiuc/professors?p={}'.format(pg), "html-parser")
-#     data = page.text
-#     soup = BeautifulSoup(data)
+for pg in range(1,170):
+    page = requests.get('https://www.koofers.com/university-of-illinois-urbana-champaign-uiuc/professors?p={}'.format(pg), "html-parser")
+    data = page.text
+    soup = BeautifulSoup(data)
+
+    profs = soup.findAll("div", {"onclick" : re.compile("go.*?")})
+
+    i = 0
+    for c in profs:
+
+        if c.text == "(adsbygoogle = window.adsbygoogle || []).push({});":
+            continue
+
+        name = c.find("div", {"class": "title"})
+        if name:
+            name = name.text.strip()
+        else:
+            name = ""
+
+        rating = c.find("div", {"class": "rating"})
+        if rating:
+            rating = rating.text.strip()
+        else:
+            rating = ""
+
+        num_ratings = c.find("div", {"class": "rating_link"})
+        if num_ratings:
+            num_ratings = num_ratings.text.strip()[:-8]
+        else:
+            num_ratings = ""
+
+        gpa = c.find("span", {"class": "value"})
+        if gpa:
+            gpa = gpa.text.strip()
+        else:
+            gpa = ""
+
+        # professor_info[name]
+        professor_info[ "{}-{}".format(pg,i)] = {"name": name, "rating":rating, "num_ratings":num_ratings, "gpa":gpa}
+        i += 1
+
+    if pg%10 == 0:
+        print("On page {}".format(pg))
+
+
+print("Number of professors found: {}".format(len(professor_info)))
+
+with open('koofers_professors.json', 'w') as fp:
+    json.dump(professor_info, fp)
